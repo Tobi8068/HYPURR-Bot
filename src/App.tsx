05d2f1f8b57cdeb0b1472076e81d9e701a7c5d51
Tobi <153672885+Tobi8068@users.noreply.github.com>
2024-12-30 05:13:20 -0500

    updated: explore page

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ export default function App() {
     <Router>
       <div className="w-full min-h-screen bg-green-100 flex flex-col relative justify-center items-center">
         <Navigation />
-        <main className="w-full px-8 py-8 bg-green-100 flex-grow overflow-hidden relative h-[calc(100vh-64px)]">
+        <main className="flex flex-col h-[calc(100vh-64px)] py-4 z-10 relative px-4 sm:px-6 xl:px-2 w-[95%]">
           <div className="absolute inset-y-0 right-1/2 z-0 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white
            dark:bg-slate-900 ring-1 ring-green-50 dark:ring-green-400 sm:-mr-80 lg:-mr-48"></div>
-          <div className='z-10 relative'>
+          <div className='flex flex-col h-full z-10'>
             <Routes>
               <Route path="/" element={<Explore />} />
               <Route path="/launches" element={<Launches />} />
